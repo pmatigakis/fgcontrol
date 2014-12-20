dee0f241abcdf3b315d58f3fafdd4a0808b94119
Panagiotis Matigakis <pmatigakis@gmail.com>
2014-12-20 14:58:07 +0200

    Added the serialVersionUID field to the RemoteFDMConnectionException class.

--- src/com/matigakis/fgcontrol/fdm/RemoteFDMConnectionException.java
+++ src/com/matigakis/fgcontrol/fdm/RemoteFDMConnectionException.java
@@ -1,6 +1,8 @@
 package com.matigakis.fgcontrol.fdm;
 
 public class RemoteFDMConnectionException extends Exception{
+	private static final long serialVersionUID = 1L;
+
 	public RemoteFDMConnectionException(String message){
 		super(message);
 	}
